aca34bf74e0cad32ca6dea00f81775edb1033305
Alfredo Inostroza <jadenguy@gmail.com>
2019-09-18 13:23:47 -0400

    Prettier colors

--- fireworks.js
+++ fireworks.js
@@ -7,12 +7,12 @@ class Firework {
         gravity = createVector(0, 1);
     }
     Init() {
-        this.fuse = Math.random() * 100;
-        this.size = Math.random() * 10;
+        this.fuse = randomRange(0, 100);
+        this.size = randomRange(5, 10);
         this.position = createVector(randomRange(0, width), height + 10);
         this.velocity = createVector(randomRange(-3, 3), randomRange(-40, -30));
         this.pastPosition = this.position;
-        this.fireColor = color(Math.random() * 100 + 100, Math.random() * 100 + 155, Math.random() * 100 + 100);
+        this.fireColor = randomRange(0, TWO_PI);
     }
     Update() {
         if (this.fuse > 0) {
@@ -23,9 +23,9 @@ class Firework {
             this.velocity.add(gravity);
             if (this.velocity.y > 0) {
                 push()
-                fill(255);
+                fill(this.GetColor(0, 255));
                 noStroke();
-                ellipse(this.position.x, this.position.y, this.size*10, this.size*10)
+                ellipse(this.position.x, this.position.y, this.size * 10, this.size * 10)
                 pop()
                 this.Init()
                 return true;
@@ -41,11 +41,17 @@ class Firework {
     }
     Draw() {
         push();
-        stroke(this.fireColor);
+        stroke(this.GetColor(50, 200));
         strokeWeight(1);
-        fill(255);
+        fill(this.GetColor(100, 200));
         line(this.pastPosition.x, this.pastPosition.y, this.position.x, this.position.y);
         ellipse(this.position.x, this.position.y, this.size);
         pop();
+    }
+    GetColor(min, max) {
+        const r = min + map(Math.sin(this.fireColor), -1, 1, 0, 1) * (max - min);
+        const g = min + map(Math.sin(this.fireColor + 2 / 3 * PI), -1, 1, 0, 1) * (max - min);
+        const b = min + map(Math.sin(this.fireColor + 4 / 3 * PI), -1, 1, 0, 1) * (max - min);
+        return color(r, g, b)
     }
 }
